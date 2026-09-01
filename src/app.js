3f269c2f5c91768d8434241cd2e5fffa325c5789
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-09-01 05:50:42 -0300

    refactor app.js to remove section navigation logic and streamline routing setup

--- src/app.js
+++ src/app.js
@@ -1,34 +1,8 @@
-const home = document.getElementById("home");
-const chat = document.getElementById("chat");
-const about = document.getElementById("about");
+import { router } from "./router.js";
+import { setupLinkInterception } from "./navigation.js";
 
-const btnHome = document.getElementById("btn-home");
-const btnChat = document.getElementById("btn-chat");
-const btnAbout = document.getElementById("btn-about");
+setupLinkInterception();
 
+window.addEventListener("popstate", router);
 
-function mostrarSeccion(seccion) {
-
-    home.classList.add("hidden");
-    chat.classList.add("hidden");
-    about.classList.add("hidden");
-
-    seccion.classList.remove("hidden");
-}
-
-
-btnHome.addEventListener("click", () => {
-    mostrarSeccion(home);
-});
-
-btnChat.addEventListener("click", () => {
-    mostrarSeccion(chat);
-});
-
-btnAbout.addEventListener("click", () => {
-    mostrarSeccion(about);
-});
-
-
-/* Vista inicial */
-mostrarSeccion(home);
+router();
