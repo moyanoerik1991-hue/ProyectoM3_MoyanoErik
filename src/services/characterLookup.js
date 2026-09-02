71b0ed4aefdd7510fd268d5f39388de9943fc344
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-09-02 08:01:29 -0300

    add toCharacterProfile function for character data formatting

--- src/services/characterLookup.js
+++ src/services/characterLookup.js
@@ -4,3 +4,14 @@ import { CHARACTERS } from "./characters.js";
 export function getCharacterById(characterId) {
   return CHARACTERS[characterId] ?? null;
 }
+
+export function toCharacterProfile(rawCharacter) {
+    const { id, name, image, presentation } = rawCharacter;
+
+    return {
+        id,
+        name: name ?? "Desconocido",
+        image: image ?? "",
+        presentation: presentation ?? "",
+    };
+}
